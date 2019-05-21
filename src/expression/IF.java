59d8cd6d103e2ba4ee9c7903beb2063246570e5f
Kowalski1337 <vadem1998@mail.ru>
2019-05-21 22:22:42 +0200

    Add IF class

---
+++ src/expression/IF.java
@@ -0,0 +1,29 @@
+package expression;
+
+import exception.DivisionByZeroException;
+
+public class IF implements Expression {
+    private Expression iff, first, second;
+
+    public IF(Expression iff, Expression first, Expression second){
+        this.first = first;
+        this.second = second;
+        this.iff = iff;
+    }
+
+    @Override
+    public int evaluate() throws DivisionByZeroException {
+        return iff.evaluate() == 1 ? first.evaluate() : second.evaluate();
+    }
+
+    @Override
+    public void generate(StringBuilder sb) {
+        sb.append("[");
+        iff.generate(sb);
+        sb.append("]?(");
+        iff.generate(sb);
+        sb.append("):(");
+        iff.generate(sb);
+        sb.append(")");
+    }
+}
